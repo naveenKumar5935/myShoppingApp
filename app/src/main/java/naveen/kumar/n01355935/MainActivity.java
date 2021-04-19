e7b2c47d0b38039fadff7f8bb6d65b7414351395
naveenkumar <naveenbti002@gmail.com>
2021-04-18 20:27:18 -0400

    login and signup screen ready

---
+++ app/src/main/java/naveen/kumar/n01355935/MainActivity.java
@@ -0,0 +1,40 @@
+package naveen.kumar.n01355935;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button login, signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.welcome_login_btn);
+        signup = findViewById(R.id.welcome_signup_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
